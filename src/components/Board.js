10e12165a68cd7ccfa3d56d1e6bee4d76265ccd8
Mike Esposito <mikeesposito6852@gmail.com>
2023-01-26 12:36:43 -0800

    Fix console log timing and rename record to takeTurn

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import pattern01 from "../imgs/pattern01.jpg"
 import pattern02 from "../imgs/pattern02.jpg"
 import pattern03 from "../imgs/pattern03.jpg"
@@ -20,6 +20,16 @@ const Board = () => {
   const [clickedArray, setClickedArray] = useState([]);
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
+  useEffect(() => {
+    shuffle(cardArray);
+  }, []);
+
+  useEffect(() => {
+    console.log("clicked array", clickedArray);
+    console.log("score", score);
+    console.log("best score", bestScore);
+  }, [clickedArray]);
+
   // shuffle function adapted from
   // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
   const shuffle = (array) => {
@@ -30,44 +40,40 @@ const Board = () => {
     setCardArray([...array]);
   }
 
-  const record = (card) => {
-    // if the card has already been clicked
-    if (clickedArray.includes(card)) {
+  const takeTurn = (card) => {
+    // if the card has not already been clicked
+    if (!clickedArray.includes(card)) {
+      // add the card to the clicked images array
+      setClickedArray(clickedArray.concat(card));
+      // increment the score
+      setScore(score + 1);
+    } else {
       // set the new best score if there is one
       if (score > bestScore) {
         setBestScore(score);
       }
       // set the current score to 0
       setScore(0);
-      // reset the clicked array
+      // reset the clicked images array
       setClickedArray([]);
-    } else {
-      // add the card to the clicked array
-      setClickedArray(clickedArray.concat(card));
-      // increment the score
-      setScore(score + 1);
     }
-    console.log("current score", score);
-    console.log("best score", bestScore);
-    console.log("clicked array", clickedArray);
-    console.log("card", card)
   }
 
   return (
     <div>
-      <img src={cardArray[0]} width={250} onClick={() => {record(cardArray[0]); shuffle(cardArray)}} />
-      <img src={cardArray[1]} width={250} onClick={() => {record(cardArray[1]); shuffle(cardArray)}} />
-      <img src={cardArray[2]} width={250} onClick={() => {record(cardArray[2]); shuffle(cardArray)}} />
-      <img src={cardArray[3]} width={250} onClick={() => {record(cardArray[3]); shuffle(cardArray)}} />
-      <img src={cardArray[4]} width={250} onClick={() => {record(cardArray[4]); shuffle(cardArray)}} />
-      <img src={cardArray[5]} width={250} onClick={() => {record(cardArray[5]); shuffle(cardArray)}} />
-      <img src={cardArray[6]} width={250} onClick={() => {record(cardArray[6]); shuffle(cardArray)}} />
-      <img src={cardArray[7]} width={250} onClick={() => {record(cardArray[7]); shuffle(cardArray)}} />
-      <img src={cardArray[8]} width={250} onClick={() => {record(cardArray[8]); shuffle(cardArray)}} />
-      <img src={cardArray[9]} width={250} onClick={() => {record(cardArray[9]); shuffle(cardArray)}} />
-      <img src={cardArray[10]} width={250} onClick={() => {record(cardArray[10]); shuffle(cardArray)}} />
-      <img src={cardArray[11]} width={250} onClick={() => {record(cardArray[11]); shuffle(cardArray)}} />
-      <img src={cardArray[12]} width={250} onClick={() => {record(cardArray[12]); shuffle(cardArray)}} />
+      <img src={cardArray[0]} width={250} onClick={() => {takeTurn(cardArray[0]); shuffle(cardArray)}} />
+      <img src={cardArray[1]} width={250} onClick={() => {takeTurn(cardArray[1]); shuffle(cardArray)}} />
+      <img src={cardArray[2]} width={250} onClick={() => {takeTurn(cardArray[2]); shuffle(cardArray)}} />
+      <img src={cardArray[3]} width={250} onClick={() => {takeTurn(cardArray[3]); shuffle(cardArray)}} />
+      <img src={cardArray[4]} width={250} onClick={() => {takeTurn(cardArray[4]); shuffle(cardArray)}} />
+      <img src={cardArray[5]} width={250} onClick={() => {takeTurn(cardArray[5]); shuffle(cardArray)}} />
+      <img src={cardArray[6]} width={250} onClick={() => {takeTurn(cardArray[6]); shuffle(cardArray)}} />
+      <img src={cardArray[7]} width={250} onClick={() => {takeTurn(cardArray[7]); shuffle(cardArray)}} />
+      <img src={cardArray[8]} width={250} onClick={() => {takeTurn(cardArray[8]); shuffle(cardArray)}} />
+      <img src={cardArray[9]} width={250} onClick={() => {takeTurn(cardArray[9]); shuffle(cardArray)}} />
+      <img src={cardArray[10]} width={250} onClick={() => {takeTurn(cardArray[10]); shuffle(cardArray)}} />
+      <img src={cardArray[11]} width={250} onClick={() => {takeTurn(cardArray[11]); shuffle(cardArray)}} />
+      <img src={cardArray[12]} width={250} onClick={() => {takeTurn(cardArray[12]); shuffle(cardArray)}} />
     </div>
   )
 }
