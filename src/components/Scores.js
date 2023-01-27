33886c6abf7b53497271d078b99b7b837a8fe402
Mike Esposito <mikeesposito6852@gmail.com>
2023-01-26 17:56:59 -0800

    Add win display if player gets all 12

--- src/components/Scores.js
+++ src/components/Scores.js
@@ -1,12 +1,20 @@
 import React from "react";
 
 const Scores = (props) => {
-  return (
-    <div>
-      SCORE:{props.score}<br></br>
-      BEST SCORE:{props.bestScore}
-    </div>
-  )
+  if (props.score === 12) {
+    return (
+      <div>
+        YOU WON!
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        SCORE:{props.score}<br></br>
+        BEST SCORE:{props.bestScore}
+      </div>
+    )
+  }
 }
 
 export default Scores
