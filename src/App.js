1e6f10057ad5e6fd561c31e2a261ecd47d89defe
Mike Esposito <mikeesposito6852@gmail.com>
2023-01-27 08:34:33 -0800

    Add title to tab

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from "react";
+import { React, useEffect } from "react";
 import Game from "./components/Game"
 
 const App = () => {
+  useEffect(() => {
+    document.title = "Pattern Picker"
+  }, []);
   return (
     <div>
       <Game />
