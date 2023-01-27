cb87baf96a9df03a60244a509020a6f4200f94b4
Mike Esposito <mikeesposito6852@gmail.com>
2023-01-27 08:44:52 -0800

    Adjust formatting of attribution

--- src/components/Attribution.js
+++ src/components/Attribution.js
@@ -12,10 +12,10 @@ const Attribution = () => {
       Image 6 by <a href="https://www.freepik.com/free-vector/flat-christmas-plaid-pattern-design_20550421.htm#query=tartan&position=12&from_view=search&track=sph#position=12&query=tartan">Freepik</a>;
       Image 7 by <a href="https://www.freepik.com/free-vector/flat-christmas-plaid-pattern-design_20286124.htm#query=blue%20square%20tartan&position=9&from_view=search&track=ais">Freepik</a>;
       Image 8 by <a href="https://www.freepik.com/free-vector/flat-christmas-plaid-pattern-design_20286279.htm#query=blue%20square%20tartan&position=18&from_view=search&track=ais">Freepik</a>;
-      Image 9 by <a href="https://www.freepik.com/free-vector/dark-blue-christmas-themed-plaid-design_20872504.htm#query=tartan%20blue&position=17&from_view=search&track=sph">Image by kjpargeter</a> on Freepik;
+      Image 9 by <a href="https://www.freepik.com/free-vector/dark-blue-christmas-themed-plaid-design_20872504.htm#query=tartan%20blue&position=17&from_view=search&track=sph">kjpargeter</a> on Freepik;
       Image 10 by <a href="https://www.freepik.com/free-vector/flat-design-sweater-like-argyle-pattern_20553019.htm#query=tartan%20blue%20square&position=4&from_view=search&track=ais">Freepik</a>;
       Image 11 by <a href="https://www.freepik.com/free-vector/flat-design-sweater-like-argyle-pattern_20552991.htm#query=tartan%20blue%20square&position=13&from_view=search&track=ais">Freepik</a>;
-      Image 12 by <a href="https://www.freepik.com/free-vector/blue-squares-cloth-background_838158.htm#query=tartan%20blue%20square&position=16&from_view=search&track=ais">Image by Layerace</a> on Freepik</h3>
+      Image 12 by <a href="https://www.freepik.com/free-vector/blue-squares-cloth-background_838158.htm#query=tartan%20blue%20square&position=16&from_view=search&track=ais">Layerace</a> on Freepik</h3>
     </div>
   )
 }
