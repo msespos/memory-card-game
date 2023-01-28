3aa53614bdd9337d468925f678335afff4dd9153
Mike Esposito <mikeesposito6852@gmail.com>
2023-01-28 14:41:36 -0800

    Add losing and other conditions announcements

--- src/components/Scores.js
+++ src/components/Scores.js
@@ -1,15 +1,28 @@
 import React from "react";
 
 const Scores = (props) => {
-  if (props.score === 12) {
+  if (props.score === 0 && props.lastScore === 0) {
+    return (
+      <div>
+        <h1>START - CLICK A PATTERN</h1>
+      </div>
+    )
+  } else if (props.score === 12) {
     return (
       <div>
         <h1>YOU WON! CLICK AGAIN TO START OVER</h1>
       </div>
     )
-  } else {
+  } else if (props.score === 0 && props.lastScore !== 12) {
     return (
       <div>
+        <h1>OOPS; YOU LOST! CLICK AGAIN TO START OVER</h1>
+      </div>
+    )
+  } else if (props.score !== 0 || props.lastScore === 12) {
+    return (
+      <div>
+        <h1>CLICK A DIFFERENT PATTERN EACH TIME</h1>
         <h2>SCORE: {props.score}</h2>
         <h2>HIGH SCORE: {props.bestScore}</h2>
       </div>
